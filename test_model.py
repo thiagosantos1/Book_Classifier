603185b2aea6e0d7c6c2a5caa3a705ba6141bfed
Thiago Santos <thiagogyn1@hotmail.com>
2018-09-07 23:38:23 -0400

    updt

---
+++ test_model.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+"""
+Created on Sept 7
+
+@author: thiago
+""" 
+
+# Importing the libraries
+import sys
+import re
+from database_process import *
+
+
+def main(): 
+
+if __name__ == '__main__':
+  main()
